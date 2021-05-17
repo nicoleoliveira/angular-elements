f1fc2f86c59e0f87749fa86d4e11714fe02f2182
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-05-17 10:07:29 -0300

    atualização

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // selector: 'app-hello-world',
@@ -9,9 +9,24 @@ export class HelloWorldComponent implements OnInit {
 
   @Input() label;
 
+  @Input() title;
+  @Input() rname;
+  @Input() occupation;
+  @Input() location;
+  @Input() first;
+
+  @Output() display = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showInfo() {
+    this.display.emit(`Name: ${this.rname}
+    Occupation: ${this.occupation}
+    Based In: ${this.location}
+    First Appearance: ${this.first}`);
   }
 
 }
